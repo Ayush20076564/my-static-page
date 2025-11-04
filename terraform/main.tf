bc2a9e400b39b010a760e100563387ff1fc72946
Ayush20076564 <20076564@mydbs.ie>
2025-11-04 22:17:35 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,42 @@
-provider "aws" {
-  region = "eu-north-1"
+###########################################
+# Terraform & Provider Configuration
+###########################################
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
 }
 
+provider "aws" {
+  region = "eu-north-1"   # ✅ use same region as your keypair
+}
+
+###########################################
+# Generate Unique Suffix for Resource Names
+###########################################
+resource "random_id" "suffix" {
+  byte_length = 3
+}
+
+###########################################
+# Use Existing SSH Key
+###########################################
 data "aws_key_pair" "web_key" {
   key_name = "hello-world"
 }
 
+###########################################
+# Security Group (with unique name)
+###########################################
 resource "aws_security_group" "web_sg" {
-  name        = "web_sg"
+  name        = "web_sg-${random_id.suffix.hex}"
   description = "Allow SSH (22) and HTTP (80)"
 
   ingress {
@@ -34,10 +63,13 @@ resource "aws_security_group" "web_sg" {
   }
 
   tags = {
-    Name = "web_sg"
+    Name = "web_sg-${random_id.suffix.hex}"
   }
 }
 
+###########################################
+# EC2 Instance
+###########################################
 resource "aws_instance" "web" {
   ami                    = "ami-0f3e4c7d7d6f5a1c4" # Ubuntu 22.04 (eu-north-1)
   instance_type          = "t3.micro"
@@ -49,7 +81,10 @@ resource "aws_instance" "web" {
   }
 }
 
+###########################################
+# Output EC2 Public IP for Ansible
+###########################################
 output "public_ip" {
-  description = "EC2 public IP"
+  description = "Public IP of the EC2 instance"
   value       = aws_instance.web.public_ip
 }
