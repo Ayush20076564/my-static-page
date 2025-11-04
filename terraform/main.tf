5c643e719fcf78947431a3ec064822c9f4a28e25
Ayush20076564 <20076564@mydbs.ie>
2025-11-04 22:44:50 +0000

    ewww

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,64 +1,57 @@
-###########################################
-# Terraform & Provider Configuration
-###########################################
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+provider "aws" {
+  region = "us-east-1c" # or your preferred regions
+}
+
+# Security group for HTTP + SSH access
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-provider "aws" {
-  region = "eu-north-1"   # or your preferred region
-}
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami           = "ami-0360c520857e3138f"  # Ubuntu 22.04 (eu-west-1)
+  instance_type = "t3.micro"
+  key_name      = "Aj website"                 # use your AWS key pair name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
-###########################################
-# Use Existing SSH Key
-###########################################
-data "aws_lightsail_key_pair" "existing" {
-  name = "hello-world"    # same name as your existing AWS key pair
-}
-
-###########################################
-# Lightsail Instance (no vCPU limits)
-###########################################
-resource "aws_lightsail_instance" "web" {
-  name              = "my-static-page"
-  availability_zone = "eu-north-1a"
-  blueprint_id      = "ubuntu_22_04"   # Ubuntu 22.04 LTS
-  bundle_id         = "nano_2_0"       # smallest plan (no quotas)
-  key_pair_name     = data.aws_lightsail_key_pair.existing.name
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y docker.io
+              systemctl enable docker
+              systemctl start docker
+              EOF
 
   tags = {
-    Name = "MyStaticPage"
+    Name = "aj-static-web"
   }
 }
 
-###########################################
-# Networking: open ports 22 + 80
-###########################################
-resource "aws_lightsail_instance_public_ports" "web_ports" {
-  instance_name = aws_lightsail_instance.web.name
-
-  port_info {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-  }
-
-  port_info {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-  }
-}
-
-###########################################
-# Outputs for CI/CD
-###########################################
+# Output the EC2 public IP
 output "public_ip" {
-  description = "Public IP of the Lightsail instance"
-  value       = aws_lightsail_instance.web.public_ip_address
+  value = aws_instance.web.public_ip
 }
